cb46fd45cf35153e401c358f03439e5852e9771b
bhanuka <rajithabhanuka1@gmail.com>
2020-06-03 19:54:05 +0530

    first commit

---
+++ src/test/java/com/java/lms/LmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
