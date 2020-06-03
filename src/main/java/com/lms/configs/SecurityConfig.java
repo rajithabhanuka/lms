94e5ccd150fd6ffba47439ee863e372e6f2c533d
bhanuka <rajithabhanuka1@gmail.com>
2020-06-03 22:02:52 +0530

    login logout created

--- src/main/java/com/lms/configs/SecurityConfig.java
+++ src/main/java/com/lms/configs/SecurityConfig.java
@@ -8,9 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class SecurityConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/home").setViewName("home");
         registry.addViewController("/login").setViewName("login");
     }
 
