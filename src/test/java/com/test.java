e1376995e6cabff3d5e6b836bafd755ae7f9c464
bhanuka <rajithabhanuka1@gmail.com>
2020-06-06 22:36:24 +0530

    quiz save validation

---
+++ src/test/java/com/test.java
@@ -0,0 +1,9 @@
+package com;
+
+public class test {
+    public static void main(String[] args) {
+        if (!false && !true && !false && !false && !false){
+            System.out.println("lol");
+        }
+    }
+}
